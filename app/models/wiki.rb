f7f2880eadb368f902d1fbde9c13f89eb374d06a
JacobYun <jacob.yun429@gmail.com>
2017-11-23 08:48:47 -0800

    made form for wikis, need to fix index and show

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,4 +1,12 @@
 class Wiki < ApplicationRecord
   belongs_to :user, optional: true
 
+  default_scope { order('created_at DESC') }
+
+  scope :visible_to, -> (user) do
+    return all if user.premium? || user.admin?
+    where(private: [false, nil])
+  end
+  
+
 end
